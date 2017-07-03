422a279d0d4db1b1060a7c8f1a12d6eb4c9ac629
Olly Shaw <ollyjshaw@gmail.com>
2017-07-03 13:18:29 +0100

    removed commit

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,5 @@
 package hello;
 
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
